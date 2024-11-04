7184fe514a6bc8aef237267c4262bef68904fce4
avouacr <avouacr@pm.me>
2024-11-04 09:53:49 +0000

    functional

--- bulk-geocode-ban.py
+++ bulk-geocode-ban.py
@@ -2,6 +2,7 @@ import os
 import math
 import requests
 import io
+import time
 
 import pandas as pd
 import pyarrow.parquet as pq
@@ -11,19 +12,19 @@ import pyarrow as pa
 ADDOK_URL = 'http://10.233.23.114:7878/search/csv/'
 
 
-def geocode_bulk(filepath_in, chunk_by_approximate_lines, requests_options):
+def geocode_bulk(filepath_in, n_rows_per_batch, requests_options):
     b = os.path.getsize(filepath_in)
     intermediate_files = []
     with open(filepath_in, 'r') as bigfile:
         row_count = sum(1 for row in bigfile)
     with open(filepath_in, 'r') as bigfile:
         headers = bigfile.readline()
-        chunk_by = math.ceil(b / row_count * chunk_by_approximate_lines)
+        chunk_by = math.ceil(b / row_count * n_rows_per_batch)
         current_lines = bigfile.readlines(chunk_by)
         i = 1
         # import ipdb;ipdb.set_trace()
         while current_lines:
-            # Send batch to addok as CSV 
+            # Send batch to addok as CSV
             current_filename = 'result-{}.csv'.format(i)
             current_csv = ''.join([headers] + current_lines)
             filename, response = post_to_addok(current_filename, current_csv, requests_options)
@@ -36,7 +37,8 @@ def geocode_bulk(filepath_in, chunk_by_approximate_lines, requests_options):
                                     "result_score": "ban_score",
                                     "result_label": "ban_label"})
             df_intermediate.to_parquet(current_filename_pq)
-            # Increment
+            # Log & increment
+            print(f"Batch {i} done ({i*n_rows_per_batch} / {row_count} rows).")
             current_lines = bigfile.readlines(chunk_by)
             i += 1
             intermediate_files.append(current_filename_pq)
@@ -62,13 +64,14 @@ def post_to_addok(filename, filelike_object, requests_options):
     return filename, response
 
 
-filename = "sample"
-# filename = "adresses_ril_achille"
-geocode_bulk(filepath_in=f'data/{filename}.csv',
-             chunk_by_approximate_lines=1000,
-             requests_options={
-                "citycode": "depcom",
-                "columns": "adresse",
-                "result_columns": "result_id"
-                }
-             )
+if __name__ == "__main__":
+    start = time.time()
+    geocode_bulk(filepath_in="data/adresses_ril_achille.csv",
+                 n_rows_per_batch=2000,
+                 requests_options={
+                     "citycode": "depcom",
+                     "columns": "adresse"
+                     }
+                 )
+    end = time.time()
+    print(f"Geocoding done in {end - start} seconds.")
